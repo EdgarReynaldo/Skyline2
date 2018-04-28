1314f2f2dc8c35fc66e5620f869b65711fa6f80d
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2018-04-28 09:05:46 -0500

    Minor change

--- src/AI.cpp
+++ src/AI.cpp
@@ -104,7 +104,6 @@ void EnemyAI::HandleEvent(EagleEvent ee) {
 void EnemyAI::CheckInputs() {
    if (mb) {
       if (mb->Ready()) {
-         float pct = Percent();
          mb->Launch(sw*Percent() , sh);
       }
    }
