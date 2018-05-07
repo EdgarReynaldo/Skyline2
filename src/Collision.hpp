e999659fb07c498de6b481bac786048f972ef804
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2018-05-06 22:07:49 -0500

    Moved all collision code here

---
+++ src/Collision.hpp
@@ -0,0 +1,66 @@
+
+
+
+
+#ifndef Collision_HPP
+#define Collision_HPP
+
+
+#include <vector>
+#include <map>
+
+
+
+struct Line {
+   int y;
+   int xmin;
+   int xmax;
+   Line(int ypos , int lx , int rx) : y(ypos) , xmin(lx) , xmax(rx) {}
+};
+
+typedef std::vector<Line> LineList;
+
+LineList CircleScanLines(int centerx , int centery , int radius);
+
+
+class Allegro5Image;
+
+class Hitmask {
+   std::vector<bool> hitmask;
+   int width;
+   int height;
+   
+   std::map<int , LineList> cmap;
+   
+   int solidmax;
+   int solidcount;
+
+public :
+   void Resize(int w , int h);
+   void ReadFromImage(Allegro5Image* a5img);
+
+   void EraseCircle(int ecx , int ecy , int radius);
+   
+   bool Hit(int x , int y) const;
+
+   int SolidCount() {return solidcount;}
+   
+   double PercentLeft() {return (double)solidcount/solidmax;}
+};
+
+
+class Missile;
+class City;
+class LaserBlast;
+
+void MissilesVsCity(std::vector<Missile*> missiles , City* city);
+
+void MissilesVsShield(std::vector<Missile*> missiles , City* city);
+
+void ExplosionsVsMissiles(std::vector<Missile*> missiles);
+
+void LasersVsMissiles(std::vector<LaserBlast*> lblasts , std::vector<Missile*> missiles);
+
+
+#endif // Collision_HPP
+
